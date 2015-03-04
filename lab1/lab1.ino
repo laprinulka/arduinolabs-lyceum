443362cb242699067d9ed96d4c7e1c90c113f773
laprinulka <yulya.laprina@gmail.com>
2015-03-04 15:43:46 +0600

    Update lab1.ino

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -5,10 +5,53 @@
 *     Дата создания:
 */
 
-void setup(){
+#define PIN_A 2
+#define PIN_B 3
+#define PIN_C 4
+#define PIN_D 5
+#define PIN_E 6
+#define PIN_F 7
+#define PIN_G 8
+#define PIN_P 9
 
+int number=0;
+
+byte digit[10] ={
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01100111,
+  0b01001110,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01110111
+};
+
+void setup() {
+  pinMode(PIN_A,OUTPUT);
+  pinMode(PIN_B,OUTPUT);
+  pinMode(PIN_C,OUTPUT);
+  pinMode(PIN_D,OUTPUT);
+  pinMode(PIN_E,OUTPUT);
+  pinMode(PIN_F,OUTPUT);
+  pinMode(PIN_G,OUTPUT);
+  pinMode(PIN_P,OUTPUT);
 }
 
 void loop(){
-
+  showDigit(number);
+  delay(1000);
+  number++;
+  if(number == 10) number = 0;
+}
+  void showDigit(int i) {
+  digitalWrite(PIN_A , digit[i]&(1<<0));
+  digitalWrite(PIN_B , digit[i]&(1<<1));
+  digitalWrite(PIN_C , digit[i]&(1<<2));
+  digitalWrite(PIN_D , digit[i]&(1<<3));
+  digitalWrite(PIN_E , digit[i]&(1<<4));
+  digitalWrite(PIN_F , digit[i]&(1<<5));
+  digitalWrite(PIN_G , digit[i]&(1<<6));
+  digitalWrite(PIN_P , digit[i]&(1<<7));
 }
