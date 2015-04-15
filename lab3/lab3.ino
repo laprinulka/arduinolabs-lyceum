1b0e8d9a0e0f00c91edd964d79a682517ce16c6f
Илья Дубков <dubkov@skbrii.ru>
2015-04-15 17:31:38 +0600

    ciferki rabotayut

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -9,14 +9,31 @@
 
 #define SS_PIN 10
 
+byte digit[10] ={
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01100111,
+  0b01001110,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01110111
+};
+
+
 void setup(){
 	pinMode(SS_PIN, OUTPUT);
+	SPI.begin();
 }
 
 void loop(){
 	for (int i=0; i<8; i++){
 		digitalWrite(SS_PIN, LOW);
-		SPI.transfer(1<<i);
+		SPI.transfer(digit[i]);
 		digitalWrite(SS_PIN, HIGH);
+		delay(500);
 	}
 }
+
+
