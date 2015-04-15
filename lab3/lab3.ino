6776596396e99759fd7e0261f168580065c08fe5
Илья Дубков <dubkov@skbrii.ru>
2015-04-15 17:00:13 +0600

    lab 3 initial

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,14 +1,22 @@
 /*
 *     Лабораторная работа 3
 *     "Бегущая строка"
-*     Автор:
-*     Дата создания:
+*     Автор: laprinulka
+*     Дата создания: 15.04.2015
 */
 
-void setup(){
+#include <SPI.h>
 
+#define SS_PIN 10
+
+void setup(){
+	pinMode(SS_PIN, OUTPUT);
 }
 
 void loop(){
-
+	for (int i=0; i<8; i++){
+		digitalWrite(SS_PIN, LOW);
+		SPI.transfer(1<<i);
+		digitalWrite(SS_PIN, HIGH);
+	}
 }
